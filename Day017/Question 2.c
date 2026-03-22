f2c26bcdc04193b2f036a966a4243fc70c68216d
Samridhi <samridhi.md696@gmail.com>
2026-03-22 17:01:10 +0530

    Update Question 2.c

--- Day017/Question 2.c
+++ Day017/Question 2.c
@@ -1,1 +1,18 @@
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
+int maxSubArray(int* nums, int numsSize) {
+    int maxSum = nums[0];
+    int currentSum = nums[0];
+
+    for(int i = 1; i < numsSize; i++) {
+        if(currentSum < 0)
+            currentSum = nums[i];
+        else
+            currentSum += nums[i];
+
+        if(currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    return maxSum;
+}
