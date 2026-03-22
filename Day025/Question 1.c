8a76c0e9cb3a3792ba0a4dccd356d0e69f29d05b
Samridhi <samridhi.md696@gmail.com>
2026-03-22 17:11:14 +0530

    Update Question 1.c

--- Day025/Question 1.c
+++ Day025/Question 1.c
@@ -1,1 +1,53 @@
+/*Problem: Count Occurrences of an Element in Linked List - Implement using linked list with dynamic memory allocation.
 
+Input:
+- First line: integer n (number of nodes)
+- Second line: n space-separated integers (linked list elements)
+- Third line: integer key (element to be counted)
+
+Output:
+- Print the number of times the key appears in the linked list*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, key;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for(int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    scanf("%d", &key);
+
+    int count = 0;
+    temp = head;
+
+    while(temp != NULL) {
+        if(temp->data == key)
+            count++;
+        temp = temp->next;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
