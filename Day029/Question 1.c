6029cbf2e17ca8897e974e237dc340078fbd8512
Samridhi <samridhi.md696@gmail.com>
2026-03-22 17:16:50 +0530

    Update Question 1.c

--- Day029/Question 1.c
+++ Day029/Question 1.c
@@ -1,1 +1,77 @@
+/*Problem: Rotate Linked List Right by k Places - Implement using linked list with dynamic memory allocation.
 
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer k
+
+Output:
+- Print the linked list elements after rotation, space-separated*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for(int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    scanf("%d", &k);
+
+    if(head == NULL || head->next == NULL) {
+        temp = head;
+        while(temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        return 0;
+    }
+
+    int length = 1;
+    temp = head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+        length++;
+    }
+
+    temp->next = head;
+
+    k = k % length;
+    int steps = length - k;
+
+    temp = head;
+    for(int i = 1; i < steps; i++) {
+        temp = temp->next;
+    }
+
+    head = temp->next;
+    temp->next = NULL;
+
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
