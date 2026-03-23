3a0cfce79ced75fad4a9484a4fded4c10241e590
Samridhi <samridhi.md696@gmail.com>
2026-03-23 09:05:02 +0530

    Update Question 2.c

--- Day024/Question 2.c
+++ Day024/Question 2.c
@@ -1,1 +1,22 @@
+//Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    dummy->next = head;
+    
+    struct ListNode* curr = dummy;
+
+    while(curr->next != NULL) {
+        if(curr->next->val == val) {
+            struct ListNode* temp = curr->next;
+            curr->next = curr->next->next;
+            free(temp);
+        } else {
+            curr = curr->next;
+        }
+    }
+
+    struct ListNode* newHead = dummy->next;
+    free(dummy);
+    return newHead;
+}
