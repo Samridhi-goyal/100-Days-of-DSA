eca686ff5ad9759f39b74b8731cb276071763992
Samridhi <samridhi.md696@gmail.com>
2026-03-23 10:17:19 +0530

    Update Question 2.c

--- Day034/Question 2.c
+++ Day034/Question 2.c
@@ -1,1 +1,38 @@
+/*Given a string s which represents an expression, evaluate this expression and return its value. 
+The integer division should truncate toward zero.
+You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().*/
 
+ #include <stdlib.h>
+#include <ctype.h>
+
+int calculate(char* s) {
+    int stack[10000];
+    int top = -1;
+
+    int num = 0;
+    char op = '+';
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        if(isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+        }
+
+        if((!isdigit(s[i]) && s[i] != ' ') || s[i+1] == '\0') {
+            if(op == '+') stack[++top] = num;
+            else if(op == '-') stack[++top] = -num;
+            else if(op == '*') stack[top] = stack[top] * num;
+            else if(op == '/') stack[top] = stack[top] / num;
+
+            op = s[i];
+            num = 0;
+        }
+    }
+
+    int result = 0;
+    for(int i = 0; i <= top; i++) {
+        result += stack[i];
+    }
+
+    return result;
+}
