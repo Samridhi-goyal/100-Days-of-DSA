1052d40ff25e19dec737d5d3889b03ba718fc85f
Samridhi <samridhi.md696@gmail.com>
2026-03-23 09:11:52 +0530

    Update Question 2.c

--- Day027/Question 2.c
+++ Day027/Question 2.c
@@ -1,1 +1,42 @@
+/*Given the head of a singly linked list, the task is to remove a cycle if present. 
+  A cycle exists when a node's next pointer points back to a previous node, forming a loop.
+  Internally, a variable pos denotes the index of the node where the cycle starts, but it is not passed as a parameter. 
+  The terminal will print true if a cycle is removed otherwise, it will print false*/
 
+class Solution {
+public:
+    int removeLoop(struct Node* head)
+    {
+        struct Node *slow = head, *fast = head;
+
+        while (slow && fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+            {
+                slow = head;
+
+                if (slow == fast)
+                {
+                    while (fast->next != slow)
+                        fast = fast->next;
+                }
+                else
+                {
+                    while (slow->next != fast->next)
+                    {
+                        slow = slow->next;
+                        fast = fast->next;
+                    }
+                }
+
+                fast->next = NULL;
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+};
