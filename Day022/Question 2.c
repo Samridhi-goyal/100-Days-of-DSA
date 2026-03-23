0867eece658bf79c4a333a65d264ab7f11160d45
Samridhi <samridhi.md696@gmail.com>
2026-03-23 09:02:20 +0530

    Update Question 2.c

--- Day022/Question 2.c
+++ Day022/Question 2.c
@@ -1,1 +1,15 @@
+//Given the head of a singly linked list, reverse the list, and return the reversed list.
 
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+
+    while(curr != NULL) {
+        struct ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+} 
