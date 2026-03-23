14a4f7c2bfbb40daba78116299086c48fa8aaba0
Samridhi <samridhi.md696@gmail.com>
2026-03-23 10:29:20 +0530

    Update Question 2.c

--- Day040/Question 2.c
+++ Day040/Question 2.c
@@ -1,1 +1,27 @@
+/*Given an array of integers temperatures represents the daily temperatures, 
+  return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
+If there is no future day for which this is possible, keep answer[i] == 0 instead.*/
 
+#include <stdlib.h>
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * temperaturesSize);
+    int* stack = (int*)malloc(sizeof(int) * temperaturesSize);
+    int top = -1;
+
+    for(int i = 0; i < temperaturesSize; i++) {
+        result[i] = 0;
+    }
+
+    for(int i = 0; i < temperaturesSize; i++) {
+        while(top >= 0 && temperatures[i] > temperatures[stack[top]]) {
+            int idx = stack[top--];
+            result[idx] = i - idx;
+        }
+        stack[++top] = i;
+    }
+
+    *returnSize = temperaturesSize;
+    free(stack);
+    return result;
+}
