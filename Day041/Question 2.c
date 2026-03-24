3aaa0e0d49b0ed33bb8a782fd051f1ec266b25fc
Samridhi <samridhi.md696@gmail.com>
2026-03-24 19:13:41 +0530

    Update Question 2.c

--- Day041/Question 2.c
+++ Day041/Question 2.c
@@ -1,1 +1,33 @@
+/*You are given an array of CPU tasks, each labeled with a letter from A to Z, and a number n. 
+Each CPU interval can be idle or allow the completion of one task. Tasks can be completed in any order, 
+but there's a constraint: there has to be a gap of at least n intervals between two tasks with the same label.
+Return the minimum number of CPU intervals required to complete all tasks.*/
 
+int leastInterval(char* tasks, int tasksSize, int n) {
+    int freq[26] = {0};
+    for (int i = 0; i < tasksSize; i++) {
+        freq[tasks[i] - 'A']++;
+    }
+    
+    int maxFreq = 0;
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > maxFreq) {
+            maxFreq = freq[i];
+        }
+    }
+    
+    int maxCount = 0;
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] == maxFreq) {
+            maxCount++;
+        }
+    }
+    
+    int partCount = maxFreq - 1;
+    int partLength = n - (maxCount - 1);
+    int emptySlots = partCount * partLength;
+    int availableTasks = tasksSize - maxFreq * maxCount;
+    int idles = emptySlots > availableTasks ? emptySlots - availableTasks : 0;
+    
+    return tasksSize + idles;
+}
