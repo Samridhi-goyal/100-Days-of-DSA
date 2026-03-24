48a8d352f501b9fba1f31cf23db4be403e3b3486
Samridhi <samridhi.md696@gmail.com>
2026-03-24 19:27:13 +0530

    Update Question 2.c

--- Day049/Question 2.c
+++ Day049/Question 2.c
@@ -1,1 +1,20 @@
+/*You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. 
+ It is guaranteed that the new value does not exist in the original BST.
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.*/
 
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if (root == NULL) {
+        struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        node->val = val;
+        node->left = node->right = NULL;
+        return node;
+    }
+    
+    if (val < root->val) {
+        root->left = insertIntoBST(root->left, val);
+    } else {
+        root->right = insertIntoBST(root->right, val);
+    }
+    
+    return root;
+}
