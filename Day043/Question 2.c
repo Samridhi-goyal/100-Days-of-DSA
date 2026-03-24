0e0e8d8dbda9e8578724b4faf197e9e2f21ea342
Samridhi <samridhi.md696@gmail.com>
2026-03-24 19:17:56 +0530

    Update Question 2.c

--- Day043/Question 2.c
+++ Day043/Question 2.c
@@ -1,1 +1,22 @@
+//Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
+#include <stdlib.h>
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int *res = (int*)malloc(10000 * sizeof(int));
+    struct TreeNode* stack[10000];
+    int top = -1, idx = 0;
+    
+    while (root != NULL || top != -1) {
+        while (root != NULL) {
+            stack[++top] = root;
+            root = root->left;
+        }
+        root = stack[top--];
+        res[idx++] = root->val;
+        root = root->right;
+    }
+    
+    *returnSize = idx;
+    return res;
+}
