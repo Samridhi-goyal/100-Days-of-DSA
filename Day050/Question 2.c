695a7bcb4786cfb40d9e849c51bfa28da0aac831
Samridhi <samridhi.md696@gmail.com>
2026-03-24 19:28:08 +0530

    Update Question 2.c

--- Day050/Question 2.c
+++ Day050/Question 2.c
@@ -1,1 +1,11 @@
+/*You are given the root of a binary search tree (BST) and an integer val.
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.*/
 
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    while (root != NULL) {
+        if (root->val == val) return root;
+        else if (val < root->val) root = root->left;
+        else root = root->right;
+    }
+    return NULL;
+}
