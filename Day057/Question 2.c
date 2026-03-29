fe840490bdef0f10821efd6aca0857d630d12b2b
Samridhi <samridhi.md696@gmail.com>
2026-03-29 11:19:28 +0530

    Update Question 2.c

--- Day057/Question 2.c
+++ Day057/Question 2.c
@@ -1,1 +1,24 @@
+/*Given the root of a binary tree, flatten the tree into a "linked list":
 
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.*/
+
+
+ void flatten(struct TreeNode* root) {
+    if (!root) return;
+
+    flatten(root->left);
+    flatten(root->right);
+
+    struct TreeNode* left = root->left;
+    struct TreeNode* right = root->right;
+
+    root->left = NULL;
+    root->right = left;
+
+    struct TreeNode* curr = root;
+    while (curr->right)
+        curr = curr->right;
+
+    curr->right = right;
+}
