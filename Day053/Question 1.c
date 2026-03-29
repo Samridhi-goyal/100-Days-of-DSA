14f3984610ec220efa592d58d1d7954bd8990069
Samridhi <samridhi.md696@gmail.com>
2026-03-29 11:01:57 +0530

    Update Question 1.c

--- Day053/Question 1.c
+++ Day053/Question 1.c
@@ -1,1 +1,97 @@
+/*Problem Statement:
+Given a binary tree, print its vertical order traversal. Nodes that lie on the same vertical line should be printed together from top to bottom and from left to right.
 
+Input Format:
+- First line contains integer N (number of nodes)
+- Second line contains N space-separated integers representing level-order traversal (-1 indicates NULL)
+
+Output Format:
+- Print nodes column by column from leftmost to rightmost vertical line*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    int hd;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int data) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = data;
+    temp->hd = 0;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    Node** queue = (Node**)malloc(n * sizeof(Node*));
+    int front = 0, rear = 0;
+
+    Node* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+    while (i < n) {
+        Node* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+void findMinMax(Node* root, int hd, int* min, int* max) {
+    if (!root) return;
+
+    if (hd < *min) *min = hd;
+    if (hd > *max) *max = hd;
+
+    findMinMax(root->left, hd - 1, min, max);
+    findMinMax(root->right, hd + 1, min, max);
+}
+
+void printVerticalLine(Node* root, int line_no, int hd) {
+    if (!root) return;
+
+    if (hd == line_no)
+        printf("%d ", root->data);
+
+    printVerticalLine(root->left, line_no, hd - 1);
+    printVerticalLine(root->right, line_no, hd + 1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    Node* root = buildTree(arr, n);
+
+    int min = 0, max = 0;
+    findMinMax(root, 0, &min, &max);
+
+    for (int i = min; i <= max; i++) {
+        printVerticalLine(root, i, 0);
+        printf("\n");
+    }
+
+    return 0;
+}
