e924427f941a84189636f7bdff975f13208225b9
Samridhi <samridhi.md696@gmail.com>
2026-03-29 11:18:05 +0530

    Update Question 2.c

--- Day056/Question 2.c
+++ Day056/Question 2.c
@@ -1,1 +1,13 @@
+//Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
+bool isMirror(struct TreeNode* left, struct TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    if (left->val != right->val) return false;
+    return isMirror(left->left, right->right) && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
