9a9f1adbb15c923f8d94c446b055bf911d51bd0a
Samridhi <samridhi.md696@gmail.com>
2026-03-31 23:35:08 +0530

    Update Question 2.c

--- Day059/Question 2.c
+++ Day059/Question 2.c
@@ -1,1 +1,43 @@
+//Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int search(int* inorder, int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct TreeNode* build(int* inorder, int* postorder, int start, int end, int* postIndex) {
+    if (start > end)
+        return NULL;
+
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = postorder[*postIndex];
+    root->left = root->right = NULL;
+    (*postIndex)--;
+
+    if (start == end)
+        return root;
+
+    int inIndex = search(inorder, start, end, root->val);
+
+    root->right = build(inorder, postorder, inIndex + 1, end, postIndex);
+    root->left = build(inorder, postorder, start, inIndex - 1, postIndex);
+
+    return root;
+}
+
+struct TreeNode* buildTree(int* inorder, int inorderSize, int* postorder, int postorderSize) {
+    int postIndex = postorderSize - 1;
+    return build(inorder, postorder, 0, inorderSize - 1, &postIndex);
+}
