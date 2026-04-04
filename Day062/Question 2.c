0a7f9cbbc74c4c48ceddf1a08b4a7b78f8ccfca3
Samridhi <samridhi.md696@gmail.com>
2026-04-04 15:56:34 +0530

    Update Question 2.c

--- Day062/Question 2.c
+++ Day062/Question 2.c
@@ -1,1 +1,32 @@
+/*There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.*/
 
+#include <stdbool.h>
+
+void dfs(int room, int** rooms, int* roomsSize, bool visited[]) {
+    visited[room] = true;
+
+    for (int i = 0; i < roomsSize[room]; i++) {
+        int key = rooms[room][i];
+        if (!visited[key]) {
+            dfs(key, rooms, roomsSize, visited);
+        }
+    }
+}
+
+bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize) {
+    bool visited[roomsSize];
+
+    for (int i = 0; i < roomsSize; i++)
+        visited[i] = false;
+
+    dfs(0, rooms, roomsColSize, visited);
+
+    for (int i = 0; i < roomsSize; i++) {
+        if (!visited[i])
+            return false;
+    }
+
+    return true;
+}
