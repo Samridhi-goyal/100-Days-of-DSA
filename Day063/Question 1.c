a77ddbbac9ecb0b40a0cf2ea359efeb1bec5c2a5
Samridhi <samridhi.md696@gmail.com>
2026-04-04 15:51:23 +0530

    Update Question 1.c

--- Day063/Question 1.c
+++ Day063/Question 1.c
@@ -1,1 +1,43 @@
+/*Problem: Perform DFS starting from a given source vertex using recursion.
 
+Input:
+- n
+- adjacency list
+- starting vertex s
+
+Output:
+- DFS traversal order*/
+
+#include <stdio.h>
+
+void dfs(int v, int n, int adj[n][n], int visited[]) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i, n, adj, visited);
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int adj[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &adj[i][j]);
+
+    int s;
+    scanf("%d", &s);
+
+    int visited[n];
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    dfs(s, n, adj, visited);
+
+    return 0;
+}
