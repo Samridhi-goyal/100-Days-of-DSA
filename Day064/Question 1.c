61b4d6dbe908ba7995f678edfea9f0427b806deb
Samridhi <samridhi.md696@gmail.com>
2026-04-05 13:44:34 +0530

    Update Question 1.c

--- Day064/Question 1.c
+++ Day064/Question 1.c
@@ -1,1 +1,52 @@
+/*Problem: Perform BFS from a given source using queue.
 
+Input:
+- n
+- adjacency list
+- source s
+
+Output:
+- BFS traversal order*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int **adj = (int **)malloc(n * sizeof(int *));
+    int *size = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &size[i]);
+        adj[i] = (int *)malloc(size[i] * sizeof(int));
+        for (int j = 0; j < size[i]; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    int s;
+    scanf("%d", &s);
+
+    int *visited = (int *)calloc(n, sizeof(int));
+    int *queue = (int *)malloc(n * sizeof(int));
+    int front = 0, rear = 0;
+
+    queue[rear++] = s;
+    visited[s] = 1;
+
+    while (front < rear) {
+        int node = queue[front++];
+        printf("%d ", node);
+        for (int i = 0; i < size[node]; i++) {
+            int v = adj[node][i];
+            if (!visited[v]) {
+                visited[v] = 1;
+                queue[rear++] = v;
+            }
+        }
+    }
+
+    return 0;
+}
