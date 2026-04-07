cebafc56c7a65e520a1b415531e88236932e39eb
Samridhi <samridhi.md696@gmail.com>
2026-04-07 23:45:58 +0530

    Update Question 2.c

--- Day066/Question 2.c
+++ Day066/Question 2.c
@@ -1,1 +1,50 @@
+/*There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.*/
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX 2000
+
+bool dfs(int node, int adj[][MAX], int size[], bool visited[], bool recStack[]) {
+    visited[node] = true;
+    recStack[node] = true;
+
+    for (int i = 0; i < size[node]; i++) {
+        int neighbor = adj[node][i];
+
+        if (!visited[neighbor]) {
+            if (dfs(neighbor, adj, size, visited, recStack))
+                return true;
+        } else if (recStack[neighbor]) {
+            return true;
+        }
+    }
+
+    recStack[node] = false;
+    return false;
+}
+
+bool canFinish(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize) {
+    int adj[MAX][MAX] = {0};
+    int size[MAX] = {0};
+    bool visited[MAX] = {false};
+    bool recStack[MAX] = {false};
+
+    for (int i = 0; i < prerequisitesSize; i++) {
+        int u = prerequisites[i][1];
+        int v = prerequisites[i][0];
+        adj[u][size[u]++] = v;
+    }
+
+    for (int i = 0; i < numCourses; i++) {
+        if (!visited[i]) {
+            if (dfs(i, adj, size, visited, recStack))
+                return false;
+        }
+    }
+
+    return true;
+}
