64928c3effe9d5e8e0ae9391db1e4e983b7123e9
Samridhi <samridhi.md696@gmail.com>
2026-04-08 15:25:18 +0530

    Add DFS function for topological ordering of DAG
    Implement DFS for topological sorting of a DAG.

--- Day067/Question 1.c
+++ Day067/Question 1.c
@@ -1,1 +1,38 @@
+//Problem: Print topological ordering of a Directed Acyclic Graph (DAG) using DFS.
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX], visited[MAX], stack[MAX];
+int n, top = -1;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+    stack[++top] = v;
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &adj[i][j]);
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    for (int i = 0; i < n; i++)
+        if (!visited[i])
+            dfs(i);
+
+    for (int i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
+
+    return 0;
+}
