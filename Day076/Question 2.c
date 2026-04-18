a715d8f31854baf8213d9f3831c2d59fa9038c77
Samridhi <samridhi.md696@gmail.com>
2026-04-18 20:09:30 +0530

    Update Question 2.c

--- Day076/Question 2.c
+++ Day076/Question 2.c
@@ -1,1 +1,27 @@
+//Clone Graph
 
+struct Node* map[101];
+
+struct Node* dfs(struct Node* node) {
+    if (node == NULL) return NULL;
+
+    if (map[node->val] != NULL) return map[node->val];
+
+    struct Node* clone = (struct Node*)malloc(sizeof(struct Node));
+    clone->val = node->val;
+    clone->numNeighbors = node->numNeighbors;
+    clone->neighbors = (struct Node**)malloc(sizeof(struct Node*) * node->numNeighbors);
+
+    map[node->val] = clone;
+
+    for (int i = 0; i < node->numNeighbors; i++) {
+        clone->neighbors[i] = dfs(node->neighbors[i]);
+    }
+
+    return clone;
+}
+
+struct Node* cloneGraph(struct Node* node) {
+    for (int i = 0; i <= 100; i++) map[i] = NULL;
+    return dfs(node);
+}
