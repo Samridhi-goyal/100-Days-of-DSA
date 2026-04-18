1b3abb61f07edbd33b64f8a793801c29fbdb115a
Samridhi <samridhi.md696@gmail.com>
2026-04-18 20:06:16 +0530

    Update Question 1.c

--- Day076/Question 1.c
+++ Day076/Question 1.c
@@ -1,1 +1,42 @@
+/*Problem Statement
+Using DFS or BFS, count number of connected components. */
 
+#include <stdio.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int visited[MAX];
+int n;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 0; i < n; i++) {
+        if (graph[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int countComponents() {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    printf("%d\n", countComponents());
+    return 0;
+}
