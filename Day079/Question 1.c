5142988bea1329d1948841bfb75b70b252873cb0
Samridhi <samridhi.md696@gmail.com>
2026-04-21 22:23:46 +0530

    Update Question 1.c

--- Day079/Question 1.c
+++ Day079/Question 1.c
@@ -1,1 +1,58 @@
+/*Problem Statement
+Find shortest distances from source vertex in a weighted graph with non-negative weights.*/
+
+#include <stdio.h>
+#include <limits.h>
+#define V 100
+
+int minDistance(int dist[], int visited[], int n)
+{
+    int min = INT_MAX, min_index = -1;
+    for (int i = 0; i < n; i++)
+        if (!visited[i] && dist[i] < min)
+            min = dist[i], min_index = i;
+    return min_index;
+}
+
+void dijkstra(int graph[V][V], int n, int src)
+{
+    int dist[V], visited[V];
+
+    for (int i = 0; i < n; i++)
+        dist[i] = INT_MAX, visited[i] = 0;
+
+    dist[src] = 0;
+
+    for (int count = 0; count < n - 1; count++)
+    {
+        int u = minDistance(dist, visited, n);
+        visited[u] = 1;
+
+        for (int v = 0; v < n; v++)
+            if (!visited[v] && graph[u][v] && dist[u] != INT_MAX &&
+                dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", dist[i]);
+}
+
+int main()
+{
+    int n, src;
+    scanf("%d", &n);
+    int graph[V][V];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
+
+    scanf("%d", &src);
+
+    dijkstra(graph, n, src);
+
+    return 0;
+}
+
 
