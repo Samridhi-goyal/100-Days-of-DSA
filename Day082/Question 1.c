722ce86bb38da81778bf72d3c83967327eb0640b
Samridhi <samridhi.md696@gmail.com>
2026-04-24 17:34:15 +0530

    Update Question 1.c

--- Day082/Question 1.c
+++ Day082/Question 1.c
@@ -1,1 +1,47 @@
+/*Problem: Given a sorted array of integers and a target value x, implement the Lower Bound and Upper Bound operations using Binary Search.
+
+Lower Bound: The index of the first element in the array that is greater than or equal to x.
+Upper Bound: The index of the first element in the array that is strictly greater than x.*/
+
+#include <stdio.h>
+
+int lower_bound(int arr[], int n, int x) {
+    int left = 0, right = n;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] < x)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+
+int upper_bound(int arr[], int n, int x) {
+    int left = 0, right = n;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] <= x)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    scanf("%d", &x);
+
+    int lb = lower_bound(arr, n, x);
+    int ub = upper_bound(arr, n, x);
+
+    printf("%d %d", lb, ub);
+    return 0;
+}
+
 
