6d44f3739262fc6f8706e34e708e4c5ddfab4cb4
Samridhi <samridhi.md696@gmail.com>
2026-04-26 09:11:15 +0530

    Update Question 2.c

--- Day084/Question 2.c
+++ Day084/Question 2.c
@@ -1,1 +1,19 @@
+/*A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+You must write an algorithm that runs in O(log n) time.*/
 
+int findPeakElement(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[mid + 1])
+            right = mid;
+        else
+            left = mid + 1;
+    }
+
+    return left;
+}
