fa38508736cd301cc24d830ef2e25cb2bdc6d4cf
Samridhi <samridhi.md696@gmail.com>
2026-04-30 18:43:07 +0530

    Update Question 2.c

--- Day086/Question 2.c
+++ Day086/Question 2.c
@@ -1,1 +1,22 @@
+/*Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.*/
 
+int mySqrt(int x) {
+    if (x == 0 || x == 1)
+        return x;
+
+    int low = 0, high = x, ans = 0;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (mid <= x / mid) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
